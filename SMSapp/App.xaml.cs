65a6e6f4ed88c4e8894f0c2bcfe7a41f906974fc
kordiand425 <98484692+kordiand425@users.noreply.github.com>
2022-07-02 01:31:33 +0200

    Add files via upload
    SMSapp

---
+++ SMSapp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppEssentials.Shared.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SMSapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+       
+            MainPage = new SmsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SMSapp/SmsPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+
+namespace AppEssentials.Shared.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
+    public partial class SmsPage : ContentPage
+    {
+        public SmsPage()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                await Sms.ComposeAsync(new SmsMessage(EntryMessage.Text, EntryNumber.Text));
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        }
+}
